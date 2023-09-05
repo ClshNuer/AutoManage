74383f0294305107f9ec724e13f384ab635b8d7d
ClshNuer <bogerdavie@foxmail.com>
2023-09-05 12:36:22 +0800

    20230905

--- automanage/src/test.py
+++ automanage/src/test.py
@@ -21,6 +21,7 @@ import subprocess
 import multiprocessing
 from io import BytesIO
 from pathlib import Path
+from datetime import datetime
 from random import randint as rand
 from random import choice as choice
 import fire
@@ -40,37 +41,3 @@ from urllib.parse import urlparse
 CUR_FOLDER = Path(__file__).parent.resolve()
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
-
-
-
-url = "http://120.78.174.238:8081/Page/MFinanceNEW/WageCalculationAccounts.aspx"
-
-nums = range(1, 4695)
-for num in nums:
-    xpath = f'//*[@id="{num}"]'
-
-raw_data = requests.get(url)
-raw_data.encoding = 'utf-8'
-content = raw_data.text
-
-tree = html.fromstring(content)
-links = tree.xpath('//a/@href')
-logger.info(links)
-
-
-
-# 创建一个浏览器对象
-browser = webdriver.Chrome()
-
-# 打开页面
-browser.get(url)
-
-# 点击弹框
-browser.find_element_by_id("mname").click()
-
-# 获取弹框中的数据
-data = browser.find_element_by_id("id_of_data_element").get_attribute("value")
-logger.info(data)
-
-# 关闭浏览器
-browser.quit()
